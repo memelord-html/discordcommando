15606771ce77411d5b79ca2e51219cd941b17675
memelord-html <54684421+memelord-html@users.noreply.github.com>
2019-08-29 20:50:14 +0200

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,7 @@
+RegisterCommand("discord", function)
+    msg("discord.gg/memelord") ----- Byt Länk Hära
+end, false)
+
+function(msg)
+    TriggerEvent("chatMessage", "BOT", {0, 147, 248}, text) ------ Byt Färger Hära
+end

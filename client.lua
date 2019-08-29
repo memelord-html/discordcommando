322dc28fb55fa071125426dbcc0818be0221b08b
HazzeS <42900417+HazzeS@users.noreply.github.com>
2019-08-29 21:10:04 +0200

    Gjorde det ännu lättare att byta discorden o fixade error

--- client.lua
+++ client.lua
@@ -1,10 +1,13 @@
+local discord = "discord.gg/memelord" -- byt länken här!
+
 RegisterCommand("discord", function()
-    msg("discord.gg/memelord") ----- Byt Länk Hära
+    msg(discord)
 end, false)
 
 function msg(text)
     TriggerEvent('chat:addMessage', {
         color = { 0 , 147, 255},
         multiline = true,
-        args = {"discord", "discord.gg/memelord"} ----- Byt Länk Hära
+        args = {"discord", discord}
     })
+end
